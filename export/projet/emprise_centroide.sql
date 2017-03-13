e4f9520faf76c6c2770d4272fd95d42cfe1f3bd9
Jean-Roc Morreale <jr.morreale@enoreth.net>
2017-03-13 10:17:52 +0100

    simplification de la vue
    suppression des infos des thésaurus et surfaces

--- export/projet/emprise_centroide.sql
+++ export/projet/emprise_centroide.sql
@@ -1,4 +1,5 @@
 -- requête pour obtenir une liste des projets avec un centroide géoréférencé
+CREATE OR REPLACE VIEW stats.projet_cda62_centroide AS 
 
 WITH source AS (
 	 SELECT
@@ -30,18 +31,8 @@ SELECT
   date_part('isoyear'::text, projet.date_fin) AS date_fin,
   projet.adresse, 
   (SELECT liste.valeur FROM app.liste WHERE liste.id = projet.id_type_projet) AS type_projet,
-  projet.raison_urgence,
-  regexp_replace(projet.problematique_recherche,  E'^\\r|\\n', '', 'g') AS problematique_recherche,
-  regexp_replace(projet.resume_scientifique, E'^\\r|^\\n', '', 'g') AS resume_scientifique ,
-  regexp_replace(projet.thesaurus_geographique, E'^\\r|^\\n', '', 'g') AS thesaurus_geographique,
-  regexp_replace(projet.thesaurus_thematique, E'^\\r|^\\n', '', 'g') AS thesaurus_thematique,
-  projet.surface_accessible,
-  projet.surface_ouverte,
-  projet.surface_pourcent,
   projet.code_oa,
-  ST_AsText(ST_Centroid(source.geom)) AS wkt_geom,
-  ST_X(ST_Centroid(source.geom)) AS x,
-  ST_Y(ST_Centroid(source.geom)) AS y
+  ST_AsText(ST_Centroid(source.geom)) AS wkt_geom
 FROM source
 LEFT JOIN app.projet ON source.id = projet.id
 ORDER by date_debut, projet.intitule;
