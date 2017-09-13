8b987ac009de750226773d455afc4383dd40910d
Jean-Roc Morreale <jr.morreale@enoreth.net>
2017-09-13 16:49:01 +0200

    analyse usage matrice geol
    - matrices avec indicatif 'primaires' en double pour une même UE
    - UE sans matrice 'primaire'

---
+++ export/ue/doublon_matrice_primaire.sql
@@ -0,0 +1,66 @@
+-- liste les UE ayant plusieurs matrices primaires
+
+WITH
+
+-- récupère les UE du projet
+ a AS (SELECT 
+	id AS "ue_id", 
+	numero 
+	FROM app.ue WHERE id_projet = 813),
+	
+-- récupère toutes les matrices géologiques
+ b AS (SELECT 
+	a.numero, 
+	id AS "mat_id", 
+	id_ue,
+	primaire
+	FROM app.matrice_geologique 
+	INNER JOIN "a" ON a.ue_id = id_ue),
+
+-- groupe par primaire is true
+c AS (
+	SELECT
+	  numero,
+	  count(numero) AS "nbr"
+	  FROM b
+	WHERE primaire IS TRUE
+	GROUP BY numero, primaire
+	ORDER BY numero)
+	
+SELECT *
+FROM c
+WHERE nbr > 1
+
+
+-- liste les UE n'ayant aucune matrice primaire
+WITH
+-- récupère les UE du projet
+ a AS (
+	SELECT 
+	id AS "ue_id", 
+	numero 
+	FROM app.ue 
+	WHERE id_projet = 846),
+	
+-- récupère toutes les matrices géologiques primaires
+ b AS (
+	SELECT 
+	a.numero, 
+	id AS "mat_id", 
+	id_ue,
+	primaire
+	FROM app.matrice_geologique 
+	JOIN "a" ON a.ue_id = id_ue
+	),
+	
+c AS (
+	SELECT b.numero
+	FROM b
+	WHERE NOT EXISTS
+	(SELECT d.id_ue, d.primaire FROM b AS d WHERE b.id_ue = d.id_ue AND d.primaire IS TRUE)
+	GROUP BY b.numero
+	ORDER BY b.numero
+	)
+
+SELECT *
+FROM c
