bdd62a5d30fbb25c77d66a5179ec600d4c716699
Jean-Roc Morreale <jr.morreale@enoreth.net>
2017-09-14 10:43:11 +0200

    ajout filtre + commentaires

--- export/regie/contenant_recolement.sql
+++ export/regie/contenant_recolement.sql
@@ -1,15 +1,28 @@
 WITH 
 
+-- récupère tous les contenants mobilier et doucmentation par projet
 source1 AS (
 SELECT DISTINCT
   contenant_mobilier.contenant_id,
   ue.id_projet
 FROM app.contenant_mobilier
 LEFT JOIN app.mobilier ON contenant_mobilier.mobilier_id = mobilier.id
-LEFT JOIN app.ue ON mobilier.id_ue = ue.id),
+LEFT JOIN app.ue ON mobilier.id_ue = ue.id
+UNION ALL
+SELECT DISTINCT
+  contenant_id,
+  id_projet
+FROM app.contenant_document
+LEFT JOIN app.document ON contenant_document.document_id = document.id
+WHERE id_projet IS NOT NULL
+),
 
+
+
+-- récupère les informations des contenants
 source2 AS (
 SELECT
+  projet.id AS id_projet,
   source1.contenant_id,
   contenant.numero,
   projet.intitule AS "intitule",
@@ -22,8 +35,10 @@ LEFT JOIN app.projet ON source1.id_projet = projet.id
 WHERE projet.intitule NOT LIKE 'intitu%' AND projet.id != 7
 ),
 
+-- récupère les valeurs des libellés
 source3 AS (
 SELECT
+  id_projet,
   source2.contenant_id,
   source2.numero,
   source2.intitule,
@@ -36,26 +51,20 @@ SELECT
   (SELECT valeur FROM app.liste WHERE liste.id = localisation.id_tablette) AS "tablette"
 FROM source2
 LEFT JOIN app.localisation ON localisation.id = source2.id_localisation
-),
-
-source_recol AS (
-SELECT id, date_recol, regexp_replace(statut, 'nouvelle localisation', 'vu', 'g') statut
-FROM maintenance.contenant_recolement
-WHERE date_recol = '2016-04-27' AND statut != 'à faire'
 )
 
+
 SELECT
+  id_projet,
   contenant_id AS id,
-  numero,
   intitule,
   matiere,
+  numero,
   "type",
   batiment,
   salle,
   travee[1],
   etagere[2],
   tablette,
-  COALESCE(source_recol.statut, 'à faire') AS statut,
-  source_recol.date_recol AS date  
+  'à faire' AS statut
 FROM source3
-LEFT JOIN source_recol ON source_recol.id = source3.contenant_id
