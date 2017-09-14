9a369a5b25c8bb4a9dc57457569c3976c2a4786c
Jean-Roc Morreale <jr.morreale@enoreth.net>
2017-09-14 11:40:32 +0200

    modifications
    - désactivation des colonnes inutiles
    - ajout d'une transformation en EPSG:2154

--- export/projet/emprise_geom_wkt.sql
+++ export/projet/emprise_geom_wkt.sql
@@ -7,17 +7,17 @@ SELECT
   projet.adresse, 
   (SELECT liste.valeur FROM app.liste WHERE liste.id = projet.id_type_projet) AS type_projet,
   projet.raison_urgence,
-  projet.problematique_recherche,
-  projet.resume_scientifique,
-  projet.thesaurus_geographique,
-  projet.thesaurus_thematique,
-  projet.surface_accessible,
-  projet.surface_ouverte,
-  projet.surface_pourcent,
-  projet.codes_ea,
+  -- projet.problematique_recherche,
+  -- projet.resume_scientifique,
+  -- projet.thesaurus_geographique,
+  -- projet.thesaurus_thematique,
+  -- projet.surface_accessible,
+  -- projet.surface_ouverte,
+  -- projet.surface_pourcent,
+  -- projet.codes_ea,
   projet.code_oa,
   date_part('isoyear'::text, projet.creation) AS creation_sia,
-  AsText(projet.the_geom)
+  ST_AsText(ST_Transform(projet.the_geom, 2154)) AS wtk_geom
 FROM app.projet
 LEFT JOIN app_addons.projet_operateur ON projet.id = projet_operateur.projet_id
 WHERE projet_operateur.organisme_id = 1
