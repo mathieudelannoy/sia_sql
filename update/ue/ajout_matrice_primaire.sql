77ec6c1f016791ae99c5389cf418448392ab7ddc
Jean-Roc Morreale <jr.morreale@enoreth.net>
2018-01-03 14:00:58 +0100

    simplication CTE

--- update/ue/ajout_matrice_primaire.sql
+++ update/ue/ajout_matrice_primaire.sql
@@ -1,31 +1,40 @@
+/*
+Le script d'export des mattrices nécessite que chaque UE n'ayant qu'une matrice ai une matrice primaire indiquée. Vu que ce n'est pas fait lors de la saisie utilisateur, cette màj le fait.
+*/
+
 UPDATE app.matrice_geologique 
 SET primaire = TRUE
 FROM (
 
 WITH
 -- récupère les UE du projet
-  a AS (
+a AS (
   SELECT 
-		id AS "ue_id", 
-		numero 
-		FROM app.ue WHERE id_projet = 836),
--- récupère toutes les matrices géologiques
-  b AS (
-  SELECT 
-		a.ue_id,
-		COUNT(a.ue_id) AS nbr
- 		FROM app.matrice_geologique 
-		INNER JOIN "a" ON a.ue_id = id_ue
-		WHERE primaire = FALSE
-		GROUP BY a.ue_id),
-c AS (
-	SELECT
-	 ue_id
-	FROM b
-	WHERE nbr = 1)
+	  id AS "ue_id", 
+	  numero 
+	FROM app.ue WHERE id_projet = 821),
 
-SELECT * FROM c) AS update_matrice
+-- récupère toutes les UE ayant une matrice primaire
+b AS (
+	SELECT a.ue_id
+	FROM a
+	JOIN app.matrice_geologique ON a.ue_id = id_ue
+	WHERE primaire = TRUE),
+
+-- récupère toutes les matrices géologiques non-primaires 
+-- dont l'UE n'a aucune primaire et qu'une matrice
+c AS (
+	SELECT 
+	  a.ue_id
+	FROM app.matrice_geologique 
+	INNER JOIN "a" ON a.ue_id = id_ue
+	WHERE primaire = FALSE
+	GROUP BY a.ue_id
+	HAVING COUNT(a.ue_id) = 1)
+
+SELECT * FROM c
+
+) AS update_matrice
 
 WHERE 
   matrice_geologique.id_ue = update_matrice.ue_id;
-  
