9d2238fc90d16e0d1804a1e331b670102fd74a2c
Jean-Roc Morreale <jr.morreale@enoreth.net>
2018-01-03 13:57:36 +0100

    export avec aggrégations des relations par type

--- export/ue/stratigraphie.sql
+++ export/ue/stratigraphie.sql
@@ -15,4 +15,26 @@ ORDER BY "c".valeur, "b".ue1 ASC
 
 
 
+-- export avec aggrégations des relations par type
 
+WITH
+
+source_strati AS (
+SELECT 
+  b.ue1 AS ue1_id, 
+  (SELECT valeur FROM app.liste WHERE liste.id = b.id_relation) AS relation,
+  array_to_string(array_agg(c.numero ORDER BY c.numero), ', ') AS ue2_agg
+FROM app.ue AS "a"
+JOIN app.relationstratigraphique as "b" ON "b".ue1 = a.id
+JOIN app.ue AS "c" ON b.ue2 = c.id
+WHERE
+  "a".id_projet = 820
+GROUP BY "b".id_relation, "b".ue1
+ORDER BY "b".id_relation, "b".ue1
+)
+
+SELECT
+  ue1_id,
+  array_to_string(array_agg(relation || ' ' || ue2_agg ORDER BY relation), '; ') AS relations
+FROM source_strati
+GROUP BY ue1_id
